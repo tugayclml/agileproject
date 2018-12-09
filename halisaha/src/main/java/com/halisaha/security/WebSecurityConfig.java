2592927aabbf427aa565e84d9a7b6c160ed705c1
tugayclml <tugay.celmeli.7@gmail.com>
2018-12-09 22:12:30 +0300

    Security files

---
+++ halisaha/src/main/java/com/halisaha/security/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.halisaha.security;
+
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.halisaha.filter.JWTAuthenticationFilter;
+import com.halisaha.filter.JWTLoginFilter;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private Environment env;
+	
+	@Override 
+	protected void configure(HttpSecurity http) throws Exception{
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and().csrf()
+				.disable().authorizeRequests().antMatchers("/staffs").access("hasRole('ROLE_ADMIN')")
+				.antMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated().and()
+				.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+
+		final CorsConfiguration configuration = new CorsConfiguration();	
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("DELETE","PUT","POST","GET"));
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(env.getProperty("usersByUsernameQuery"))
+			.authoritiesByUsernameQuery(env.getProperty("authoritiesByUsernameQuery")).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.debug(true);
+	}
+}
